071b80e3ebc2ab563ba54ebb9a270bc469b3dde7
sbulacio <svbulacio@gmail.com>
2020-01-07 19:04:15 -0300

    Selenium con Java - clase 2

---
+++ PersonalWorkspace/src/Introduction/DefaultVariables.java
@@ -0,0 +1,27 @@
+package Introduction;
+
+public class DefaultVariables {
+
+	static byte byteVariable;
+	static short shortVariable;
+	static int intVariable ;
+	static long longVariable;
+	static float floatVariable;
+	static double doubleVariable;
+	static boolean boolVariable;
+	static char chariVariable;  //you can assign the ascii value to a char variable
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("byte value: "+byteVariable);
+		System.out.println("short value; "+shortVariable);
+		System.out.println("int value: "+intVariable);
+		System.out.println("long value: "+longVariable);
+		System.out.println("float value:"+floatVariable);
+		System.out.println("double value: "+doubleVariable);
+		System.out.println("double value: "+boolVariable);
+		System.out.println("char value: " + chariVariable);
+	}
+
+}
---
+++ PersonalWorkspace/src/Introduction/Variables.java
@@ -0,0 +1,33 @@
+package Introduction;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte byteVariable = 100;
+		System.out.println("byte value: "+byteVariable);
+		
+		short shortVariable = 1000;
+		System.out.println("short value; "+shortVariable);
+		
+		int intVariable = 10000;
+		System.out.println("int value: "+intVariable);
+		
+		long longVariable = -200000000L;
+		System.out.println("long value: "+longVariable);
+		
+		float floatVariable = 1234.13f;
+		System.out.println("float value:"+floatVariable);
+		
+		double doubleVariable = 321.225;
+		System.out.println("double value: "+doubleVariable);
+		
+		boolean boolVariable = true;
+		System.out.println("double value: "+boolVariable);
+		
+		char chariVariable = 'S';  //you can assign the ascii value to a char variable
+		System.out.println("char value: " + chariVariable);
+	}
+
+}
